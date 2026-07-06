a3ba2416a0fbbddd0b02d243803123067287092c
mehradxppp8 <mehradgholami.9721@gmail.com>
2026-07-06 10:27:35 +0330

    config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    ADMIN_PASSWORD: process.env.ADMIN_PASS || "admin", // رمز پیش‌فرض
+    SUB_PATH: process.env.SUB_PATH || "sub", // مسیر لینک سابسکریپشن
+    PANEL_NAME: "AIPAN"
+};
